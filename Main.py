c31773c242ccafd4a3b25ddcfe4c2f91905c2cfe
João Pedro Amaral Rosa <joaopedro251900@gmail.com>
2024-05-06 13:44:23 -0300

    reajuste codigo

--- Main.py
+++ Main.py
@@ -1,18 +1,46 @@
 # Criar a janela principal
 import tkinter as tk
-from TransformarIMGparaExcel import select_image
+from TransformarExcelEmIMG import ImageEditorApp, open_image
+from TransformarIMGparaExcel import IMGExcel, select_image_convert
 
+# Função para comparar RGB e CMYK
+def compare_rgb_cmyk():
+    convert = ImageEditorApp(root);
+    if app.selected_color:
+        rgb_color = app.selected_color
+        cmyk_color = convert.rgb_para_cmyk(rgb_color)
+        status.set(f"RGB: {rgb_color}   CMYK: {cmyk_color}")
 
+# Inicializar a aplicação
 root = tk.Tk()
-root.title("Selecione uma imagem pequena pelo amor de Deus!")
-#setta um novo formato 300 por 100
-root.geometry("700x700")
+root.title("Editor de Imagem")
+
+# Barra de status para exibir a cor do pixel sob o cursor do mouse
+status = tk.StringVar()
+status_label = tk.Label(root, textvariable=status, bd=1, relief=tk.SUNKEN, anchor=tk.W)
+status_label.pack(side=tk.BOTTOM, fill=tk.X)
+
+app = ImageEditorApp(root)
+IMGExc = IMGExcel(root)
+
+# Menu
+menu_bar = tk.Menu(root)
+file_menu = tk.Menu(menu_bar, tearoff=0)
+file_menu.add_command(label="Sair", command=root.quit)
+menu_bar.add_cascade(label="Arquivo", menu=file_menu)
+root.config(menu=menu_bar)
+
+panel = tk.Label(root)
 
 # Botão para selecionar uma imagem
-select_button = tk.Button(root, text="Gerar Excel", command=select_image)
-select_button.pack(pady=20)
+select_button = tk.Button(root, text="Gerar Excel", command=lambda:(select_image_convert(IMGExc), open_image(app)))
+select_button.pack(pady=5)
 
-# Executar o loop principal da interface gráfica
-root.mainloop() #função do TK tlgd
+compare_button = tk.Button(root, text="Comparar RGB e CMYK", command=compare_rgb_cmyk)
+compare_button.pack(pady=5)
 
-#Se o arquivo for grande essa porra demora para cacete!
+root.status = status  # Para acessar a barra de status na classe ImageEditorApp
+
+root.mainloop()
+
+
--- TransformarIMGparaExcel.py
+++ TransformarIMGparaExcel.py
@@ -1,3 +1,4 @@
+
 from tkinter import filedialog
 from PIL import Image
 from openpyxl import Workbook
@@ -8,99 +9,103 @@ from colormath.color_objects import sRGBColor, CMYKColor
 from colormath.color_conversions import convert_color
 
 def rgb_to_cmyk(rgb):
-    # Convertendo o valor RGB para sRGBColor
-    rgb_color = sRGBColor(rgb[0] / 255, rgb[1] / 255, rgb[2] / 255)
-    # Convertendo de sRGB para CMYK
-    cmyk_color = convert_color(rgb_color, CMYKColor)
-    # Retornando os valores CMYK normalizados entre 0 e 100
-    return [cmyk_color.cmyk_c * 100, cmyk_color.cmyk_m * 100, cmyk_color.cmyk_y * 100, cmyk_color.cmyk_k * 100]
+        # Convertendo o valor RGB para sRGBColor
+        rgb_color = sRGBColor(rgb[0] / 255, rgb[1] / 255, rgb[2] / 255)
+        # Convertendo de sRGB para CMYK
+        cmyk_color = convert_color(rgb_color, CMYKColor)
+        # Retornando os valores CMYK normalizados entre 0 e 100
+        return [cmyk_color.cmyk_c * 100, cmyk_color.cmyk_m * 100, cmyk_color.cmyk_y * 100, cmyk_color.cmyk_k * 100]
 
-def image_to_excel_cmyk(image_path, excel_path):
-    # Leitura da imagem
-    img = cv2.imread(image_path)
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    
-    # Criando um novo arquivo Excel
-    wb = Workbook()
-    ws_cmyk = wb.active
-    ws_cmyk.title = "CMYK"
+class IMGExcel:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Editor de Imagem")
 
-    # Iterando sobre cada pixel da imagem e adicionando os valores CMYK ao Excel
-    for row, row_pixels in enumerate(img_rgb):
-        for col, pixel in enumerate(row_pixels):
-            # Convertendo os valores RGB para CMYK
-            cmyk_values = rgb_to_cmyk(pixel)
-            # Escrevendo os valores CMYK na célula correspondente
-            ws_cmyk.cell(row=row + 1, column=col + 1).value = f"{cmyk_values[0]}, {cmyk_values[1]}, {cmyk_values[2]}, {cmyk_values[3]}"
-
-    # Salvando o arquivo Excel
-    wb.save(excel_path)
-    print("Imagem convertida para Excel (CMYK) com sucesso!")
-
-
-
-def image_to_excelRGB(image_path, excel_path):
-    # Leitura da imagem
-    img = cv2.imread(image_path)
-    # Conversão para RGB (OpenCV lê em BGR)
-    wb = Workbook()
-    ws_rgb = wb.active
-    ws_rgb.title = "RGB"
-
-    # Iterando sobre cada pixel da imagem e adicionando os valores RGB ao Excel
-    for row, row_pixels in enumerate(img):
-        for col, pixel in enumerate(row_pixels):
-            # Obtendo os valores RGB do pixel
-            r, g, b = pixel
-            # Escrevendo os valores RGB na célula correspondente
-            ws_rgb.cell(row=row + 1, column=col + 1).value = f"{r}, {g}, {b}"
-
-    # Salvando o arquivo Excel
-    wb.save(excel_path)
-    print("Imagem convertida para Excel (RGB) com sucesso!")
-
-
-def image_to_matrix_gray(image_path):
-    try:
-            # Abre a imagem
-        image = Image.open(image_path)
-        # Converte a imagem em escala de cinza
-        grayscale_image = image.convert('L')
-        # Obtém as dimensões da imagem
-        width, height = grayscale_image.size
-        # Obtém os valores dos pixels da imagem usando numpy para eficiência
-        pixel_values = np.array(grayscale_image.getdata(), dtype=np.uint8).reshape((height, width))
-        return pixel_values
-    except Exception as e:
-        print("Erro ao converter imagem para matriz:", e)
-        return None
-
-def save_matrix_to_excel_gray(matrix, excel_filename):
-    try:
-        # Cria um arquivo Excel
+    def image_to_excel_cmyk(self,image_path, excel_path):
+        # Leitura da imagem
+        img = cv2.imread(image_path)
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        
+        # Criando um novo arquivo Excel
         wb = Workbook()
-        ws = wb.active
-        # Escreve os valores da matriz no arquivo Excel
-        for row in matrix:
-            ws.append(list(row))  # Convertendo numpy.ndarray para lista
-        # Salva o Excel
-        wb.save(excel_filename)
-        #deu certo
-        print("Imagem convertida para Excel (Gray) com sucesso!")
-    except Exception as e:
-        print("Erro fudeu tudo: ", e)
+        ws_cmyk = wb.active
+        ws_cmyk.title = "CMYK"
 
-def select_image():
-        # Pega a imagem
-        file_path = "IMG\madacasca.jpg"
+        # Iterando sobre cada pixel da imagem e adicionando os valores CMYK ao Excel
+        for row, row_pixels in enumerate(img_rgb):
+            for col, pixel in enumerate(row_pixels):
+                # Convertendo os valores RGB para CMYK
+                cmyk_values = rgb_to_cmyk(pixel)
+                # Escrevendo os valores CMYK na célula correspondente
+                ws_cmyk.cell(row=row + 1, column=col + 1).value = f"{cmyk_values[0]}, {cmyk_values[1]}, {cmyk_values[2]}, {cmyk_values[3]}"
 
+        # Salvando o arquivo Excel
+        wb.save(excel_path)
+        print("Imagem convertida para Excel (CMYK) com sucesso!")
+
+    def image_to_excelRGB(self, image_path, excel_path):
+        # Leitura da imagem
+        img = cv2.imread(image_path)
+        # Conversão para RGB (OpenCV lê em BGR)
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # Criando um novo arquivo Excel
+        wb = Workbook()
+        ws_rgb = wb.active
+        ws_rgb.title = "RGB"
+
+        # Iterando sobre cada pixel da imagem e adicionando os valores RGB ao Excel
+        for row, row_pixels in enumerate(img_rgb):
+            for col, pixel in enumerate(row_pixels):
+                # Obtendo os valores RGB do pixel
+                r, g, b = pixel
+                # Escrevendo os valores RGB na célula correspondente
+                ws_rgb.cell(row=row + 1, column=col + 1).value = f"{r}, {g}, {b}"
+
+        # Salvando o arquivo Excel
+        wb.save(excel_path)
+        print("Imagem convertida para Excel (RGB) com sucesso!")
+
+    def image_to_matrix_gray(self, image_path):
+        try:
+            # Abre a imagem
+            image = Image.open(image_path)
+            # Converte a imagem em escala de cinza
+            grayscale_image = image.convert('L')
+            # Obtém as dimensões da imagem
+            width, height = grayscale_image.size
+            # Obtém os valores dos pixels da imagem usando numpy para eficiência
+            pixel_values = np.array(grayscale_image.getdata(), dtype=np.uint8).reshape((height, width))
+            return pixel_values
+        except Exception as e:
+            print("Erro ao converter imagem para matriz:", e)
+            return None
+
+    def save_matrix_to_excel_gray(self, matrix, excel_filename):
+        try:
+            # Cria um arquivo Excel
+            wb = Workbook()
+            ws = wb.active
+            # Escreve os valores da matriz no arquivo Excel
+            for row in matrix:
+                ws.append(list(row))  # Convertendo numpy.ndarray para lista
+            # Salva o Excel
+            wb.save(excel_filename)
+            #deu certo
+            print("Imagem convertida para Excel (Gray) com sucesso!")
+        except Exception as e:
+            print("Erro fudeu tudo: ", e)
+
+def select_image_convert(app):
         # Verifica se a imagem foi selecionada
+        file_path = "IMG/winx.png"
+
         if file_path:
-            image_gray = image_to_matrix_gray(file_path)
-            image_to_excelRGB(file_path,"saidaRGB.xlsx")
-            image_to_excel_cmyk(file_path,"saidaCMYK.xlsx")
+            image_gray = app.image_to_matrix_gray(file_path)
+            app.image_to_excelRGB(file_path,"saidaRGB.xlsx")
+            app.image_to_excel_cmyk(file_path,"saidaCMYK.xlsx")
             if image_gray is not None:
-                save_matrix_to_excel_gray(image_gray, "saidaGray.xlsx")
+                app.save_matrix_to_excel_gray(image_gray, "saidaGray.xlsx")
 
             else:
                 print("Não foi possível processar a imagem.")
